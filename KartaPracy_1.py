b03d5556ca0e46013cc1c56999f6287dfb09e91d
24kp <115844791+kkkaaarrr@users.noreply.github.com>
2022-10-15 00:50:55 +0200

    Add files via upload

---
+++ KartaPracy_1.py
@@ -0,0 +1,44 @@
+print("KARTA PRACY NR 1")
+
+print("Zad 1")
+a = int(input("Wpisz pierwszą liczbę:"))
+b = int(input("Wpisz drugą liczbę:"))
+wynik1 = a**2 + b**2
+print(wynik1)
+
+print("Zad 2")
+a = int(input("Wpisz liczbę:"))
+b = int(input("Wpisz liczbę:"))
+wynik2 = (a+b)**2
+print(wynik2)
+
+print("Zad 3")
+a = int(input("Wpisz:"))
+b = int(input("Wpisz:"))
+wynik3 = (a-b)**3
+print(wynik3)
+
+print("Zad 4")
+a = int(input("Wpisz:"))
+b = int(input("Wpisz:"))
+c = int(input("Wpisz:"))
+wynik4 = a*b*c
+print(wynik4)
+
+print("Zad 5")
+a = int(input("Wpisz:"))
+b = int(input("Wpisz:"))
+wynik5 = (a+b)*0.4
+
+print(wynik5)
+
+print("Zad 6")
+x = int(input("Wpisz cenę brutto:"))
+print(x/1.23)
+
+print("Zad 7")
+
+a = int(input("Wpisz:"))
+b = int(input("Wpisz:"))
+reszta = a%b
+print(reszta)
---
+++ KartaPracy_2.py
@@ -0,0 +1,56 @@
+# print("Zadanie 1")
+# a = int(input("Liczba:"))
+# if a % 3 == 0:
+#     print("Tak")
+# else:
+#     print("Nie")
+    
+# print("Zadanie 2")
+# a = int(input("Liczba:"))
+# if a >= 100 and a < 1000 and a % 17 == 0:
+#     print("Tak")
+# else:
+#     print("Nie")
+
+# print("Zadanie 3")
+# wiek = int(input("Wpisz swój wiek:"))
+# if wiek >= 18:
+#     print("Tak")
+# else:
+#     print("Nie")
+
+# print("Zadanie 4")
+# waga = int(input("Waga ciężarówki:"))
+# LIMIT = int(20)
+# if waga <= LIMIT:
+#     print("Tak")
+# else:
+#     print("Nie")
+
+# print("Zadanie 5")
+# a = int(input("Liczba:"))
+# b = int(input("Liczba:"))
+# c = int(input("Liczba:"))
+# if c < b and c > a:
+#  print("Tak")
+# else:
+#  print("Nie")
+#
+# print("Zadanie 6")
+# a = int(input("Liczba:"))
+# p = int(input("Liczba:"))
+# if a % p == 0 and a**p % p == 0:
+#  print("Tak")
+# else:
+#  print("Nie")
+
+print("Zadanie 7")
+p = int(input("Punkt startu:"))
+k = int(input("Pukt końcowy:"))
+s = int(input("Długość skoku Bajtożabki:"))
+zabrak = ((k - p) - s*3) * -1
+przekroczenie = s*3 - (k - p)
+if s*3 >= k - p:
+    print("Tak, BajtoZabce zabrakło ", zabrak, " skoków do pokonania dystansu.")
+else:
+    print("Nie, Bajtożaba przekroczyła swój dystans o", przekroczenie, " skoków.")
