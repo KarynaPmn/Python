19b89c37df4426a743e59cf75ca240a46a6a8c14
24kp <115844791+RichyKap@users.noreply.github.com>
2022-10-25 22:14:12 +0200

    Dodano KartaPracy_3b

---
+++ KrtaPracy_3b.py
@@ -0,0 +1,88 @@
+# print("Zadanie 1")
+# for i in range(1, 32):
+#     print(i, "listopada 2022")
+
+# print("Zadanie 2")
+# n2 = int(input("Podaj liczbę: "))
+# for i in range(0, n2*2):
+#     if i % 2 == 1:
+#         print(i*i)
+
+# print("Zadanie 3")
+# for i in range(1000,10000):
+#     if i % 379 == 0:
+#         print(i)
+
+# print("Zadanie 4")
+# for i in range(100,1000):
+#     if i % 5 == 0 or i % 6 == 0 or i % 11 == 0:
+#         print(i)
+
+# print("Zadanie 5")
+# n5 = int(input("Ile chcesz podać liczb?: "))
+# suma = 0
+# for i in range(0,n5):
+#     l = int(input("Podaj liczbę:"))
+#     suma += l
+# print("Suma podanych liczb wynosi:", suma)
+
+# print("Zadanie 6")
+# k = int(input("Podaj liczbę:"))
+# suma6 = 0
+# for i in range(0, k):
+#     if i % 2 == 0:
+#         suma6 += i
+# print(suma6)
+
+# print("Zadanie 7")
+# m = int(input("Podaj liczbę dwucfrową:"))
+# suma7 = 0
+# if m >= 10 and m < 100:
+#     for i in range(10,m):
+#         if i % 2 == 1:
+#             suma7 += i
+#     print(suma7) 
+# else:
+#     print("Liczba nie jest dwucyfrowa.")
+
+
+
+
+
+print("Zadanie 8")
+W = int(input("Podaj kwotę: "))
+L = int(input("Podaj lata inwestycji:"))
+wart = W
+wartM = 0
+for i in range(0, L * 12):
+    wartM = wart * 0.06 * (1/12)
+    wart += wartM
+print("Wartość inwestycji wynosi", round(wart, 2), "zł.")
+
+# print("Zadanie 9")
+# n = int(input("Podaj ile chcesz liczb:"))
+# k = 21
+# suma = 0
+# for i in range(0,n):
+#     print(k)
+#     suma += k
+#     k += 100
+
+
+# from math import sqrt
+
+# print("Zadanie 10")
+# for i in range(1,100):
+#     sqrt(i) % i == 0
+
+# from math import sqrt
+
+
+# print("Zadanie 10")
+# for i in range(1, 1000):
+#     if i % 10 == sqrt(i):
+#         print(i)
+#     elif i % 100 == sqrt(i):
+#         print(i)
+#     elif i % 1000 == sqrt(i):
+#         print(i)
