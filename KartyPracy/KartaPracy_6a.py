2bb5288c38d207cc92d136822dd812165a3a1a95
22kp <115844791+RichyKap@users.noreply.github.com>
2022-12-12 13:05:10 +0000

    dod. zad. 2

--- KartyPracy/KartaPracy_6a.py
+++ KartyPracy/KartaPracy_6a.py
@@ -6,14 +6,27 @@
 #   x + n // 10
 # print(suma)
 
-print("Zadanie 3")
-n = int(input())
-suma = 0
+# print("Zadanie 2")
+# n = int(input())
+# flaga = True
+# for i in range(2, n):
+#   if n % i == 0:
+#     flaga = False
+# if flaga:
+#   print("Liczba jest piewsza.")
 
-for i in range(1,n)
-  if n % i == 0:
-    suma += i
-if suma == n:
-  print("Liczba jest doskonała.")
-else:
-  print("Liczba nie jest doskonała.")
+# print("Zadanie 3")
+# n = int(input())
+# suma = 0
+
+# for i in range(1,n)
+#   if n % i == 0:
+#     suma += i
+# if suma == n:
+#   print("Liczba jest doskonała.")
+# else:
+#   print("Liczba nie jest doskonała.")
+
+print("Zadanie 4")
+
+
