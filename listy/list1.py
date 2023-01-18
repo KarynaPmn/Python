eeb76f07a66611291c0d3923b14d607dbcdf9839
22kp <115844791+RichyKap@users.noreply.github.com>
2023-01-18 07:51:37 +0000

    DOD. ZAD

--- listy/list1.py
+++ listy/list1.py
@@ -1,90 +1,91 @@
 x = list(range(5))
-# for item in x:
-# 	print(item)
+for item in x:
+	print(item)
 
-# 	for item2 in range(4):
-# 		print(item2)
+	for item2 in range(4):
+		print(item2)
 
-# print(len(x))
+print(len(x))
 
-# for i in range(len(x)):
-# 	print(x[i])
+ for i in range(len(x)):
+ 	print(x[i])
 
-## DEKLARACJA LISTY I ITERACJA  PO LIŚCIE
+# DEKLARACJA LISTY I ITERACJA  PO LIŚCIE
 
-# L = [3, 5, 8, 13, 17, 27]
+ L = [3, 5, 8, 13, 17, 27]
 
-# for elem in L:
-# 	print(elem, end=" ")
-# print("\n")
+for elem in L:
+	print(elem, end=" ")
+print("\n")
 
-# for i in range(len(L)):
-# 	print(L[i], end=" ")
+for i in range(len(L)):
+	print(L[i], end=" ")
 
-# ## FUNKCJE SPECJALNE NA TABLICACH
+## FUNKCJE SPECJALNE NA TABLICACH
 K = [4, 7, 5, 7, 3, 3, 2, 8, 7]
 print(K)
 
-# #apend() - dodaje element do listy
-# K.append(3)
-# print(K)
+#apend() - dodaje element do listy
+K.append(3)
+print(K)
 
-# #count() - zwraca ile jest elementów
-# print(K.count(7))
+#count() - zwraca ile jest elementów
+print(K.count(7))
 
-# #index() - zwraca gdzie jest pierwszy element
-# print(K.index(7))
+#index() - zwraca gdzie jest pierwszy element
+print(K.index(7))
 
-# #insert() - 
-# K.insert(2, 4)
-# print(K)
+#insert() - 
+K.insert(2, 4)
+print(K)
 
-# #JAK WSTAWIĆ 4 NA KONIEC LISTY?
-# K.insert(len(K), 4) # lub po prostu append()
-# print(K)
+#JAK WSTAWIĆ 4 NA KONIEC LISTY?
+K.insert(len(K), 4) # lub po prostu append()
+print(K)
 
-# #pop() - domyślnie usuwa ostatnią
-# K.pop()
-# print(K)
+#pop() - domyślnie usuwa ostatnią
+K.pop()
+print(K)
 
-# #reverse()
-# K.reverse()
-# print(K)
+#reverse()
+K.reverse()
+print(K)
 
-# #sort(reverse = true) - true to malejąco
-# K.sort()
-# print(K)
+#sort(reverse = true) - true to malejąco
+K.sort()
+print(K)
 
-# # ZADANIE 1. USUŃ WSZYSTKIE 7 Z LISTY (remove, pop i index, count)
-# # SPOSUB 1
-# for i in range(K.count(7)):
-# 	K.remove(7)
-# print(K)
-# #  SPOSUB 2 
-# for i in range(K.count(7)):
-# 	K.pop(K.index(7))
-# print(K)
+# ZADANIE 1. USUŃ WSZYSTKIE 7 Z LISTY (remove, pop i index, count)
 
-# # ZADANIE 2. POSZUKAM MAKSA W 2 METODAMI
+# SPOSUB 1
+for i in range(K.count(7)):
+	K.remove(7)
+print(K)
+#  SPOSUB 2 
+for i in range(K.count(7)):
+	K.pop(K.index(7))
+print(K)
 
-# # SPOSÓB 1
-# print(max(K))
+## ZADANIE 2. POSZUKAM MAKSA W 2 METODAMI
 
-# # SPOSÓB 2
-# maksik = K[0]
-# for i in K:
-# 	if i > maksik:
-# 		maksik = i
-# print(maksik)
+# SPOSÓB 1
+print(max(K))
 
-# # SPOSÓS 3. PSEUDOKOD!
-# maksik = K[0]
-# for i in range(len(K)):
-# 	if K[i] > maksik:
-# 		maksik = K[i]
-# print(maksik)
+# SPOSÓB 2
+maksik = K[0]
+for i in K:
+	if i > maksik:
+		maksik = i
+print(maksik)
 
-##
+# SPOSÓS 3. PSEUDOKOD!
+maksik = K[0]
+for i in range(len(K)):
+	if K[i] > maksik:
+		maksik = K[i]
+print(maksik)
+
+## ZWRACA Z LISTY
 L = [3, 7, 2, 1, 5, 4, 2, 4, 8, 6]
 print(L[1:4]) # [7, 2, 1]]
 print(L[:3]) # [3, 7, 2]
