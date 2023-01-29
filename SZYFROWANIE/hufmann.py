5dec4be6c37042fcc5be2403b05c62527ba5933d
22kp <115844791+RichyKap@users.noreply.github.com>
2023-01-29 22:27:07 +0100

    Create hufmann.py

---
+++ SZYFROWANIE/hufmann.py
@@ -0,0 +1,20 @@
+# kod hufmana
+
+W = "ABCCCDDDDDEFGGGHHIJJ"
+E = "AB3C5DEF3G2HI2J"
+
+H = ""
+ilosc = 1 
+for i in range(len(W)-1):
+  if W[i] == W[i+1]:
+    ilosc += 1
+  else:
+    if ilosc > 1:
+      H += str(ilosc)
+    H += W[i]
+    ilosc = 1
+if ilosc > 1:
+  H += str(ilosc)
+H += W[len(W)-1]
+print(W)
+print(H)
