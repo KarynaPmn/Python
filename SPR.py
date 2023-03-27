3de15183c82cb7281d5b705b300b66417f94af46
22kp <115844791+RichyKap@users.noreply.github.com>
2023-03-27 12:15:47 +0000

    Zadanie 5

--- SPR.py
+++ SPR.py
@@ -1,7 +1,46 @@
 #  1. Wczytaj dowolny napis i wypisz z niego pierwszą i ostatnią literkę
 
-n_1 = str(input('Podaj napis: '))
-print(f"{n_1[0]} i {n_1[-1]}" )
+# n_1 = str(input('Podaj napis: '))
+# print(f"{n_1[0]} i {n_1[-1]}" )
 
 # 2. Wczytaj dowolny napis i wypisz z niego literki bez pierwszej i ostatniej
 
+# n_2 = input('Wpisz: ')
+# print(n_2[1:-1])
+# print(n_2[1 : len(n_2) - 1])
+# for i in range(1, len(b) - 1):
+# 	print(n_2, end=' ')
+	
+# 3. Wypisz 4 ostatnie literki z wpisanego napisy w kolejności od końca
+
+# c = input()
+# print(c[-1:-5:-1])
+# print(c[:-5:-1])
+
+# for i in range(len(c) - 1):
+# 	print(c, end=" ")
+
+# C =list(c)
+# C.reverse()
+# c = "".join(C)
+# print(c[:4])
+
+# 4. Waga napisu to suma kodów ascii jego liter. Zważ wpisny napis
+
+# d = input()
+# s = 0
+
+# for i in d:
+# 	s += ord(i)
+# print(s)
+
+# 5. Policz ile we wpisanym napisie jest liter A.
+
+e = input()
+s = 0
+
+for i in e:
+	if i == "a":
+		s += 1
+
+print(s)
