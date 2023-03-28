a0c445f30ecca5c2f3bb3c0ea93ffd7a60a0bd2c
22kp <115844791+RichyKap@users.noreply.github.com>
2023-03-28 18:39:27 +0200

    Update pre.py

--- Cezar/pre.py
+++ Cezar/pre.py
@@ -16,13 +16,12 @@
 # for i in range(len(napis)):
 #   szyfr += chr(ord(napis[i]) + 3)
 
-# Zad 1 
-a, b, c = int(input()),int(input()), int(input())
-iloczyn = a * b
 
-while b != a:
-  if a > b: a -= b
-  if b > a: b -= a
+// 
 
-print(iloczyn // a)
+napis = input()
+szyfr = ""
+for i in range(len(napis)):
+    szyfr = szyfr + chr(65 + (ord(napis[i]) - 65 + 3) % 26)
+print(napis, szyfr)
 
