fc16dc12a8f8c70714b145b8988442df63eb576a
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-10-22 11:22:17 +0200

    Update 06_PrzetwarzaniePlikowIoperacjeNaNapisach.py

--- KLASA_3/INF/06_PrzetwarzaniePlikowIoperacjeNaNapisach.py
+++ KLASA_3/INF/06_PrzetwarzaniePlikowIoperacjeNaNapisach.py
@@ -1,6 +1,7 @@
 import random
 import statistics
 
+
 # Zad 1
 def Zad_1():
     userFirstname = input("Podaj imię: ")
@@ -17,7 +18,7 @@ def Zad_1():
 def Zad_2():
     plik = open("dane_osobowe.txt", "r")
     DaneOsobowe = plik.readlines()
-    
+
     for i in range(0, len(DaneOsobowe), 2):
         imie = DaneOsobowe[i].strip()
         nazwisko = DaneOsobowe[i + 1].strip()
@@ -26,10 +27,10 @@ def Zad_2():
 
     plik.close()
 
-# Zad 3 
+# Zad 3
 def LosoweLiczbyDoPliku(path):
     plik = open(path, "w")
-    
+
     for i in range(10):
         plik.write(str(random.randint(1, 100)) + "\n")
 
@@ -54,5 +55,36 @@ def Zad_3():
     print(f"Suma liczb: {SumaLiczb(LosoweLiczby)}")
     print(f"Max: {max(LosoweLiczby)}")
     print(f"Min: {min(LosoweLiczby)}")
-    print(f"Średnia: {statistics.mean(LosoweLiczby)}") # statistics.mean() --> oblicza średnią liczb z listy
+    print(f"Średnia: {statistics.mean(LosoweLiczby)}")  # statistics.mean() --> oblicza średnią liczb z listy
 
+# Zad 4
+def Zad_4():
+    plik = open("ciagi.txt", "r")
+
+    ileCiag = 0
+    for ciag in plik:
+        suma = len(ciag.split())
+
+        if suma % 2 == 0:
+            ileCiag += 1
+
+    print(f"W ciągu jest: {ileCiag}")
+    plik.close()
+
+# Zad 5 -- Nie zakończone !!!
+def Zad_5():
+    plik = open("slowa.txt", "r")
+    Slowa = plik.readlines()
+
+    print("1. wyświetli ponumerowane słowa z pliku tekstowego")
+    print("2. wyświetli liczbę słów w pliku")
+    print("3. wyświetli słowa zaczynające się na literę A")
+    print("4. wyświetli słowa kończące się na literę A")
+    print("5. wyświetli słowa oraz liczbę liter, z których się składają")
+    print("6. *wyświetli najkrótsze oraz najdłuższe słowo w pliku oraz ich długość (jeżeli jest ich kilka wyświetl pierwsze z nich)")
+    print("7. wyświetli słowa o długości 6")
+    print("8. wyświetli słowa zawierające literę O oraz dla każdego z tych słów liczbę tych liter O")
+    print("9. wyświetli ile razy w całym pliku występuje litera A")
+    number = input("Podaj wybór: ")
+
+    plik.close()
