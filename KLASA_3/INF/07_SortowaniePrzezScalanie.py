e2d2538ee944b6e284180c5089975b1a423a8861
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-11-04 19:42:03 +0100

    Update 07_SortowaniePrzezScalanie.py

--- KLASA_3/INF/07_SortowaniePrzezScalanie.py
+++ KLASA_3/INF/07_SortowaniePrzezScalanie.py
@@ -1,10 +1,9 @@
-# Nie zakończone !!!
 def Scal(T, lewy, srodek, prawy):
     i = lewy
     j = srodek + 1
     k = lewy
 
-    Pom = []
+    Pom = [0] * len(T)
     while i <= srodek and j <= prawy:
         if T[i] < T[j]:
             Pom[k] = T[i]
@@ -13,18 +12,19 @@ def Scal(T, lewy, srodek, prawy):
             Pom[k] = T[j]
             j += 1
         k += 1
-        while i <= srodek:
-            Pom[k] = T[i]
-            i += 1
-            k += 1
 
-        while j <= prawy:
-            Pom[k] = T[j]
-            j += 1
-            k += 1
+    while i <= srodek:
+        Pom[k] = T[i]
+        i += 1
+        k += 1
 
-        for i in range(lewy, prawy):
-            T[i] = Pom[i]
+    while j <= prawy:
+        Pom[k] = T[j]
+        j += 1
+        k += 1
+
+    for i in range(lewy, prawy + 1):
+        T[i] = Pom[i]
 
 def Sortuj(T, lewy, prawy):
     if lewy < prawy:
@@ -33,10 +33,41 @@ def Sortuj(T, lewy, prawy):
         Sortuj(T, srodek + 1, prawy)
         Scal(T, lewy, srodek, prawy)
 
+def Scal2(T):
+    if len(T) > 1:
+        mid = len(T) // 2
+        L = T[:mid]
+        R = T[mid:]
+        
+        Scal2(L)
+        Scal2(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                T[k] = L[i]
+                i += 1
+            else:
+                T[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            T[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            T[k] = R[j]
+            j += 1
+            k += 1
+
 # Zad 1
 def Zad_1():
     T = list(map(int, input("Podaj ciąg: ").split(" ")))
-    Pom = Sortuj(T, 0, len(T) - 1)
-    print(Pom)
+
+    Sortuj(T, 0, len(T) - 1)
+    print(T)
 
 Zad_1()
