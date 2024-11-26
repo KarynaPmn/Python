bb3807836cdd3a75571cae3b82df25a638803773
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-11-26 11:21:47 +0100

    Update 09_ZnajdowaniePodciagowORoznychWlasciwosciach.py

--- KLASA_3/INF/09_ZnajdowaniePodciagowORoznychWlasciwosciach.py
+++ KLASA_3/INF/09_ZnajdowaniePodciagowORoznychWlasciwosciach.py
@@ -1,10 +1,12 @@
 # Zad 1
+def WyswietlLiczbyPoSpacji(Tab):
+    print(' '.join(map(str, Tab)))
 def Podciagi(Ciag):
     n = len(Ciag)
 
     for dl in range(1, n + 1):
         for indexPocz in range(n - dl + 1):
-            print(Ciag[indexPocz : indexPocz + dl])
+            WyswietlLiczbyPoSpacji(Ciag[indexPocz : indexPocz + dl])
 
 def Zad_1():
     plik = open("liczby.txt", "r")
