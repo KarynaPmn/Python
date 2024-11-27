36c02e15affcf42388b9dcccda5eab077784bd65
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-11-27 15:06:54 +0100

    Update 09_ZnajdowaniePodciagowORoznychWlasciwosciach.py

--- KLASA_3/INF/09_ZnajdowaniePodciagowORoznychWlasciwosciach.py
+++ KLASA_3/INF/09_ZnajdowaniePodciagowORoznychWlasciwosciach.py
@@ -1,6 +1,10 @@
+plik = open("liczby.txt", "r")
+Ciag = list(map(int, plik.read().split()))
+
 # Zad 1
 def WyswietlLiczbyPoSpacji(Tab):
     print(' '.join(map(str, Tab)))
+
 def Podciagi(Ciag):
     n = len(Ciag)
 
@@ -8,10 +12,43 @@ def Podciagi(Ciag):
         for indexPocz in range(n - dl + 1):
             WyswietlLiczbyPoSpacji(Ciag[indexPocz : indexPocz + dl])
 
-def Zad_1():
-    plik = open("liczby.txt", "r")
-    Ciag = list(map(int, plik.read().split()))
+def Podciagi_1(Ciag):
+    n = len(Ciag)
 
-    Podciagi(Ciag)
+    for dl in range(1, n + 1):
+        for indexPocz in range(n - dl + 1):
+            indexKon = indexPocz + dl - 1
 
-Zad_1()
+            for i in range(indexPocz, indexKon + 1):
+                print(Ciag[i], end=' ')
+            print()
+
+# Zad 2
+def Zad_2(Ciag):
+    n = len(Ciag)
+
+    for indexPocz in range(n + 1):
+        for i in range(indexPocz, n + 1):
+            WyswietlLiczbyPoSpacji(Ciag[indexPocz : i])
+
+# Zad 3 !!! nie dokończone
+def SumaCyfr(s):
+    suma = 0
+
+    for i in range(len(s)):
+        a = int(s[i])
+        while a > 0:
+            suma += a % 10
+            a //= 10
+
+    return suma
+def Zad_3(Ciag):
+    n = len(Ciag)
+
+    for indexPocz in range(n - 4):
+        for i in range(indexPocz, indexPocz + 6):
+            print(SumaCyfr(Ciag[indexPocz: i]), end=" - ")
+            WyswietlLiczbyPoSpacji(Ciag[indexPocz : i])
+
+# Zad 4 
+Zad_3(Ciag)
