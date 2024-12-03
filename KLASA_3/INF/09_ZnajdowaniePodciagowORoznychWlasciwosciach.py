84bd9ba997c7c76dac7a27676aa9fdc6d6289a94
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-12-03 11:19:28 +0100

    Update 09_ZnajdowaniePodciagowORoznychWlasciwosciach.py

--- KLASA_3/INF/09_ZnajdowaniePodciagowORoznychWlasciwosciach.py
+++ KLASA_3/INF/09_ZnajdowaniePodciagowORoznychWlasciwosciach.py
@@ -31,7 +31,7 @@ def Zad_2(Ciag):
         for i in range(indexPocz, n + 1):
             WyswietlLiczbyPoSpacji(Ciag[indexPocz : i])
 
-# Zad 3 
+# Zad 3
 def SumaCyfr(s):
     suma = 0
 
@@ -51,4 +51,70 @@ def Zad_3(Ciag):
             WyswietlLiczbyPoSpacji(Ciag[indexPocz : i])
 
 # Zad 4
-Zad_3(Ciag)
+# Zad 5
+
+# Zad 6 i 9
+def CzyCiagJestRosnacy(C):
+    for i in range(len(C) - 1):
+        if C[i] > C[i + 1]:
+            return False
+    return True
+
+def Zad_6_9():
+    C = list(map(int, input("Podaj ciąg po spacji: ").split(" ")))
+
+    if CzyCiagJestRosnacy(C):
+        print("Tak")
+    else:
+        print("Nie")
+
+# Zad 7 i 8
+def CzyCiagJesMalejacy(C):
+    for i in range(len(C) - 1):
+        if C[i] < C[i + 1]:
+            return False
+    return True
+
+def Zad_7_8():
+    C = list(map(int, input("Podaj ciąg po spacji: ").split(" ")))
+
+    if CzyCiagJesMalejacy(C):
+        print("Tak")
+    else:
+        print("Nie")
+
+# Zad 10
+def CzyCiagJestMonotoniczny(C):
+    for i in range(1, len(C)):
+        if C[1] != C[i]:
+            return False
+    return True
+
+def Zad_10():
+    C = list(map(int, input("Podaj ciąg po spacji: ").split(" ")))
+
+    if CzyCiagJestMonotoniczny(C):
+        print("Tak")
+    else:
+        print("Nie")
+
+# Zad 11
+def NajdluzszyCiagJestRosnacy(C):
+    N = []
+    Temp = []
+
+    for i in range(len(C) - 1):
+        if C[i] < C[i + 1]:
+            Temp.append(C[i])
+        else:
+            if len(Temp) > len(N):
+                N = Temp
+            Temp = []
+    return N
+    
+def Zad_11():
+    print(len(NajdluzszyCiagJestRosnacy(Ciag)))
+
+# Zad 12
+def Zad_12():
+    print(NajdluzszyCiagJestRosnacy(Ciag))
