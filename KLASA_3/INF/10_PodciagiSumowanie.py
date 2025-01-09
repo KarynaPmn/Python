57ee96f7b197135c0175ea7dfc7bfa65f92dfb36
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2025-01-09 07:38:55 +0100

    Update 10_PodciagiSumowanie.py

--- KLASA_3/INF/10_PodciagiSumowanie.py
+++ KLASA_3/INF/10_PodciagiSumowanie.py
@@ -14,4 +14,16 @@ def Zad_1(Ciag):
 
     print(maksSuma)
 
-Zad_1(Ciag)
+def Zad_2(Ciag):
+    maksSuma = 0
+    suma = 0
+    T = []
+
+    for i in range(len(Ciag) - 2):
+        suma = Ciag[i] + Ciag[i + 1] + Ciag[i + 2]
+
+        if maksSuma < suma and not maksSuma == suma:
+            maksSuma = suma
+            T = [Ciag[i], Ciag[i + 1], Ciag[i + 2]]
+
+    print(T)
