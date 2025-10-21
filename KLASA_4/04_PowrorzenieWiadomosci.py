9c62bc47a2c41e1e2471829a4f4b08853eb4de08
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-10-21 19:03:12 +0200

    Update 04_PowrorzenieWiadomosci.py

--- KLASA_4/04_PowrorzenieWiadomosci.py
+++ KLASA_4/04_PowrorzenieWiadomosci.py
@@ -1,34 +1,73 @@
+from math import ceil
+import sys
 
-from itertools import count
+
+def AlgorytmZachlanny(Nominaly, kwota):
+    iloscNominalow = 0
+
+    for nominal in Nominaly:
+        ile = kwota // nominal
+        iloscNominalow += ile
+        kwota %= nominal
+
+    return iloscNominalow
+
+
 def Zad_1():
     plik = open("dane.txt", "r")
     Nominaly = list(map(int, plik.readline().split()))
     Kwoty = list(map(int, plik.readlines()))
-    minIloscNominalul = float("Inf")
-    minIlN = []
-    minList = []
+    minIloscNominslow = sys.maxsize
+
+    plik.close()
 
     for kwota in Kwoty:
-        N = []
-        ileNominalow = 0
-        temp = kwota
-        for nominal in Nominaly:
-            ileM = 0
-            while (temp - nominal > 0):
-                ileM += 1
-                ileNominalow += 1
-                N.append(nominal)
-                temp -= nominal
+        iloscNominalow = AlgorytmZachlanny(Nominaly, kwota)
 
-        if (ileNominalow < minIloscNominalul):
-            minIloscNominalul = ileNominalow
-            minList.append(kwota)
-            minIlN.append(ileNominalow)
+        if iloscNominalow < minIloscNominslow:
+            minIloscNominslow = iloscNominalow
+    
+    print("Najmniejszą ilość nominałów (", minIloscNominslow ,") ma: ", end="")
+    for kwota in Kwoty:
+        if AlgorytmZachlanny(Nominaly, kwota) == minIloscNominslow:
+            print(kwota, end=" ")
+    print()
 
-    # Nie zakończone!
-    # if (minIlN.count(minIlN[-1])):
-        # for _ in range(len())
+def Zad_2():
+    M = []
+    suma = 0
+    kierunek = ""
+    with open("macierz.txt", "r") as plik:
+        M = [list(map(int, linia.split())) for linia in plik]
+    
+    
 
-    print(minList[-1])
+    n = len(M)
+    i = 0
+    j = 0
+    while i < n - 1 or j < n - 1:
+        if i == n - 1:
+            j += 1
+            kierunek += "P"
+        elif j == n - 1:
+            i += 1
+            kierunek += "D"
+        elif M[i][j + 1] > M[i + 1][j]:
+            j += 1
+            kierunek += "P"
+        else:
+            i += 1
+            kierunek += "D"
 
-Zad_1()
+        suma += M[i][j]
+
+    for t in M:
+        for e in t:
+            print(e, end="\t")
+        print()
+        
+    print(kierunek)
+    print(suma)
+        
+
+Zad_2()
