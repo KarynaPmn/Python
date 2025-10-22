51fea990ca1f86458e73dd255f92d4db41932848
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-10-22 16:48:18 +0200

    Update 05_ProgramowanieDynamiczne_WydawanieReszty.py

--- KLASA_4/05_ProgramowanieDynamiczne_WydawanieReszty.py
+++ KLASA_4/05_ProgramowanieDynamiczne_WydawanieReszty.py
@@ -1,1 +1,18 @@
 
+def MinimalnaLiczbaNominaluw(nominaly, kwota, n):
+    MAX = kwota + 1
+    LN = []
+    nominal = 0
+    for i in range(kwota):
+        LN[i] = MAX
+    LN[0] = 0
+    for i in range(kwota):
+        for j in range(n - 1):
+            nominal = nominaly[j]
+            if i >= nominal:
+                if LN[i - nominal] + 1 < LN[i]:
+                    LN[i] = LN[i - nominal] + 1
+    if LN[kwota] != MAX:
+        return LN[kwota]
+    else:
+        return -1
