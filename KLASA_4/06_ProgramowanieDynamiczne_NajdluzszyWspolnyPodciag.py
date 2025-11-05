a0268b670e9d6e023d007302baacaf6c517df46e
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-11-05 16:50:14 +0100

    Update 06_ProgramowanieDynamiczne_NajdluzszyWspolnyPodciag.py

--- KLASA_4/06_ProgramowanieDynamiczne_NajdluzszyWspolnyPodciag.py
+++ KLASA_4/06_ProgramowanieDynamiczne_NajdluzszyWspolnyPodciag.py
@@ -4,9 +4,9 @@ def NajdluzszyWspolnyPodciag(X, Y):
     m = len(Y)
     D = [[0 for j in range(m + 1)] for i in range(n + 1)]
 
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         D[i][0] = 0
-    for j in range(m + 1):
+    for j in range(1, m + 1):
         D[0][j] = 0
     for i in range(1, n):
         for j in range(1, m):
@@ -21,7 +21,7 @@ def NajdluzszyWspolnyPodciag(X, Y):
     N = [[0 for j in range(m + 1)] for i in range(n + 1)] # zawiera znaki najdłuższego wspólnego podciągu w odpowiedniej kolejności.
     while i > 0 and j > 0:
         if X[i - 1] == Y[j - 1]:
-            N[k] = X[i]
+            N[k] = X[i - 1]
             k -= 1
             i -= 1
             j -= 1
