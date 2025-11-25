59680d63ce98f3c2d17a741271ce17efdeda31cd
Karyna Pomin <115844791+KarynaPmn@users.noreply.github.com>
2025-11-25 19:26:07 +0100

    Refactor NajdluzszyWspolnyPodciag function

--- KLASA_4/06_ProgramowanieDynamiczne_NajdluzszyWspolnyPodciag.py
+++ KLASA_4/06_ProgramowanieDynamiczne_NajdluzszyWspolnyPodciag.py
@@ -1,15 +1,9 @@
-# Nie zakończone !!!
 def NajdluzszyWspolnyPodciag(X, Y):
-    n = len(X)
-    m = len(Y)
-    D = [[0 for j in range(m + 1)] for i in range(n + 1)]
+    n, m = len(X), len(Y)
+    D = [[0] * (n + 1) for _ in range(m + 1)]
 
-    for i in range(1, n + 1):
-        D[i][0] = 0
-    for j in range(1, m + 1):
-        D[0][j] = 0
-    for i in range(1, n):
-        for j in range(1, m):
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
             if X[i - 1] == Y[j - 1]:
                 D[i][j] = D[i - 1][j - 1] + 1
             else:
@@ -30,13 +24,13 @@ def NajdluzszyWspolnyPodciag(X, Y):
         else:
             j -= 1
 
-    return D[n][m] # długość najdłuższego ciągu
+    return D[n][m]
 
 def Zad_2():
     # X = list(map(int, input("Podaj 1 ciąg liczb całkowitych: ").split()))
     # Y = list(map(int, input("Podaj 2 ciąg liczb całkowitych: ").split()))
 
-    NajdluzszyWspolnyPodciag([1, 2, 3, 4], [2, 3, 4, 5])
+    print(NajdluzszyWspolnyPodciag([1, 3, 4, 1, 2, 8], [3, 4, 1, 2, 1, 7])) # Wynik: 4
 
 Zad_2()
 
